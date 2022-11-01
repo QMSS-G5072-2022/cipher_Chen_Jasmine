704eaf177f2702e2b8085a2dcb210201707e3cc9
Jasmine Chen <sc5088@columbia.edu>
2022-11-01 11:21:49 -0400

    hw07 package - test

--- cipher_sc5088/tests/test_cipher_sc5088.py
+++ cipher_sc5088/tests/test_cipher_sc5088.py
@@ -1,1 +1,8 @@
 from cipher_sc5088 import cipher_sc5088
+
+def test_cipher_negative_shift():
+    shift_eg1 = -8
+    text_eg1= 'Python is so fun'
+    assert cipher_sc5088.cipher(text_eg1,shift_eg1)=='HqlZgf ak kg Xmf', 'shift can be negative'
+
+
